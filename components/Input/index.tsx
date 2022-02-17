715ec2eaf06ccb951e7a9f9ee4bdbc9373659b2a
reactcraft <reactcraft@outlook.com>
2022-02-17 23:08:42 +0000

    fix: input styles

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -3,11 +3,16 @@ import type InputProps from './InputProps';
 import Icon from '../Icon';
 
 const Input = ({ icon, placeholder, ariaLabel }: InputProps) => (
-  <div className="relative">
-    {icon && <Icon className="absolute left-3 top-3.5" icon={icon} />}
+  <div className="group relative">
+    {icon && (
+      <Icon
+        className="text-muted absolute left-3 top-3.5 group-focus-within:text-primary-500"
+        icon={icon}
+      />
+    )}
     <input
       className={classNames(
-        'bg-muted block w-full rounded-full py-3.5 pr-3 text-sm focus:outline-primary-500',
+        'bg-muted block w-full rounded-full py-3.5 pr-3 text-sm focus:bg-transparent focus:outline-none focus:ring-1 focus:ring-primary-500',
         {
           'pl-11': icon,
           'pl-3': !icon,
