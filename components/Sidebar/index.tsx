084e2a93b8503362760c500ca5a54df0978fdbd7
reactcraft <reactcraft@outlook.com>
2022-02-17 18:47:13 +0000

    feat: add vertical padding to sidebar

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import type SidebarProps from './SidebarProps';
 
 const Sidebar = ({ className, children }: SidebarProps) => (
-  <aside className={classNames('xl:p-3', className)}>{children}</aside>
+  <aside className={classNames('py-3 xl:p-3', className)}>{children}</aside>
 );
 
 export default Sidebar;
